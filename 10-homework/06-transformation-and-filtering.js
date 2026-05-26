aee85a5539b026d2e6c88b59b1ee3c443f99782c
JeStdGit <j0556799880@gmail.com>
2026-05-26 16:25:54 +0300

    Revert "10 homework"

--- 10-homework/06-transformation-and-filtering.js
+++
@@ -1,7 +0,0 @@
-const products = [{ id: 1, price: 20 }, { id: 2, price: 50 }, { id: 3, price: 15 }]
-
-const cost20AndUp = products.filter(product => product.price >= 20)
-console.log(cost20AndUp);
-
-const cost20AndUpIds = cost20AndUp.map(product => product.id)
-console.log(cost20AndUpIds);
